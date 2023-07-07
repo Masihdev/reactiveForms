e6d34e350b15d0dc057d114eef83f6bccd129ac7
Masihdev <masihullahmassudi85@gmail.com>
2023-07-07 17:23:40 +0200

    add html for the form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'reactiveForms';
+
+  gender = [
+    { id: '1', value: 'Male' },
+    { id: '2', value: 'Female' },
+    { id: '3', value: 'Other' }
+  ]
 }
